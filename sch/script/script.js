df5ce4043ada306e03ffd6c47bb893215724bdbb
dsummatech <jahtech1@gmail.com>
2024-01-27 11:39:12 +0100

    new

--- sch/script/script.js
+++ sch/script/script.js
@@ -6,4 +6,9 @@ $(function () {
    $(".mobile_menu .fa-times-circle").click(function () {
     $(".mobile_menu").fadeOut();
    })
+
+   $(".fa-eye").click(function () {
+      $(".bal").toggle();
+      $(".demo_bal").toggle();
+   });
 });
