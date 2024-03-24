89cfd1a0675617c0a687f41945d93c6207dc7e06
dsummatech <jahtech1@gmail.com>
2024-03-25 00:34:40 +0100

    add

--- lovelymkt/main.js
+++ lovelymkt/main.js
@@ -3,14 +3,28 @@ $(function () {
     function slider() {
         if (sl < 80) {
             sl+=20;
-            $(".slide").css("right",sl + "%");
+            $(".slider .slide").css("right",sl + "%");
         }
         else{
             sl=0;
-            $(".slide").css("right",sl + "%");
+            $(".slider .slide").css("right",sl + "%");
         }
         
     }
     setInterval(slider, 2000);
+
+    var ofsl = 0;
+    function offers_slider() {
+        if (ofsl < 50) {
+            ofsl+=25;
+            $(".offers-slider .slide").css("right",ofsl + "%");
+        }
+        else{
+            ofsl=0;
+            $(".offers-slider .slide").css("right",ofsl + "%");
+        }
+        
+    }
+    setInterval(offers_slider, 2000);
     
 });
